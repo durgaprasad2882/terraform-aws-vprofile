944081db6b31b5b199ffc23f7e591c42fae173c0
durgaprasad2882 <dm.prasad@hotmail.com>
2023-06-21 17:02:22 +0530

    repeat

--- bean-env.tf
+++ bean-env.tf
@@ -1,8 +1,8 @@
-resource "aws_elastic_beanstalk_environment" "vrofile-bean-prod" {
+resource "aws_elastic_beanstalk_environment" "vprofile-bean-prod" {
   name                = "vprofile-bean-prod"
   application         = aws_elastic_beanstalk_application.vprofile-prod.name
   solution_stack_name = "64bit Amazon Linux 2 v4.3.8 running Tomcat 8.5 Corretto 11"
-  cname_prefix        = "vprofile-bean-prod-domain"
+  cname_prefix        = "vprofile-bean-prod-domain-demo"
   setting {
     name      = "VPCId"
     namespace = "aws:ec2:vpc"
