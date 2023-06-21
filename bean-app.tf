4c86f2a8c402a7bb9dbd3e6fec7090cd67f9db7a
durgaprasad2882 <dm.prasad@hotmail.com>
2023-06-21 16:19:08 +0530

    bog commit mm

--- bean-app.tf
+++ bean-app.tf
@@ -1,3 +1,3 @@
-resource "aws_elastic_beanstalk_application" "aws-vprofile-prod" {
-  name = "aws-vprofile-prod"
+resource "aws_elastic_beanstalk_application" "vprofile-prod" {
+  name = "vprofile-prod"
 }
